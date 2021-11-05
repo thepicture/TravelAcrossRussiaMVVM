35c27c52bb279011a70266e721938129923e9fe8
thepicture <fuzzyeastern@protonmail.com>
2021-11-05 07:46:47 +0000

    Implemented ViewModelBase

---
+++ Views/MainViewModel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace TravelAcrossRussiaMVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainViewModel : Window
+    {
+        public MainViewModel()
+        {
+            InitializeComponent();
+        }
+    }
+}
