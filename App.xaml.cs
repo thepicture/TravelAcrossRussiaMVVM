55af61c7ade739f08736737e85e90388f50fae0b
thepicture <fuzzyeastern@protonmail.com>
2021-11-05 08:55:26 +0000

    Created initial design for the ToursView

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using TravelAcrossRussiaMVVM.ViewModels;
 
 namespace TravelAcrossRussiaMVVM
 {
@@ -13,5 +8,14 @@ namespace TravelAcrossRussiaMVVM
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainView
+            {
+                DataContext = new ToursViewModel()
+            };
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
     }
 }
